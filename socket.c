977d111db2bbf435bd3b9e90a9263bac5928dd72
James Koenig <Viscous.cycle@gmail.com>
2013-03-06 02:25:25 -0800

    wrapping up the bsd-socket listener setup code

--- socket.c
+++ socket.c
@@ -5,7 +5,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/wait.h>
 
 #include <netinet/in.h>
 #include <netdb.h>
@@ -37,12 +36,47 @@ struct addrinfo * getLocalAddressInfo(int port)
     /* populate the linked list */
     if(addrErr = getaddrinfo(NULL, port, &hint, &localInfo))
     {
+        /* error returned by function call, print it */
         fprintf(stdout, "getaddrinfo: %s\n", gai_strerror(addrErr));
         return NULL;
     }
     else
     {
+        /* call successful */
         return localInfo;
+    }
+}
+
+//Function to create and bind a listener socket based on 
+//a passed address value 
+//returns a socket file descriptor on success, 0 on failure.
+int bindToAddress(struct addrinfo * addr)
+{
+    int sfd = 0;
+    int optVal = 1;
+    if(addr = NULL) return 0; //code equivalent of foreshadowing
+    /* The if call from hell.  Feel free to make fun of me for this, or change it yourself */
+    else if(
+            // open the socket file descriptor
+            (sfd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol)) != -1 &&
+            // set the socket to keep alive 
+            setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &optVal, sizeof(int))       != -1 &&
+            setsockopt(sfd, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof(int))       != -1 &&
+            bind(sfd, addr->ai_addr, addr->ai_addrlen)                            != -1
+           )
+    {
+        //if that massive if-statement went through, that means we're home free and the socket
+        //is bound properly. return the socket descriptor
+        return sfd;
+    }
+    else 
+    {
+        //there's like 5 ways this function could've gone wrong, let's at least print one
+        perror("BindToAddress");
+        //close the socket file descriptor
+        close(sfd);
+        //call this function on the next member of the list THAT'S RIGHT, RECURSION!
+        return bindToAddress(addr->next);
     }
 }
 
@@ -56,17 +90,7 @@ int listenOnPort(int port)
    addrInfo = getLocalAddressInfo(port);
    if(!addrInfo) return 0;
    
-   /* iterate through the linked list and bind to the first socket possble */
-   for(iter_p = addrInfo; iter_p != NULL; iter_p = iter_p->next)
-   {
-       /* try to set up the socket file descriptor */
-       if((sock_fd = socket(iter_p->ai_family, iter_p->ai_socktype, iter_p->ai_protocol)) == -1)
-       {
-           perror("listenOnPort: socket");
-           continue;
-       }
-       /* */
-   }
+   sock_fd = bindToAddress(addrInfo);
 
    return sock_fd;
 }
